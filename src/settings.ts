b38960587ebcccddd0f180d154ff1cd614f97a49
clockblocker <asde3211q@gmail.com>
2025-03-02 06:34:39 +0100

    remove reload button from settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,6 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, PluginSettingTab, Setting } from 'obsidian';
 import TextEaterPlugin from './main';
-import { ApiService } from './api';
-import { Editor, MarkdownView } from 'obsidian';
-import { Notice } from 'obsidian';
-import fillTemplate from './commands/fillTemplate';
-import getInfinitiveAndEmoji from './commands/getInfinitiveAndEmoji';
-import normalizeSelection from './commands/normalizeSelection';
-import translateSelection from './commands/translateSelection';
-import formatSelectionWithNumber from './commands/formatSelectionWithNumber';
-import checkRuDeTranslation from './commands/checkRuDeTranslation';
+
 
 export class SettingsTab extends PluginSettingTab {
     plugin: TextEaterPlugin;
@@ -24,12 +16,12 @@ export class SettingsTab extends PluginSettingTab {
         containerEl.empty();
 
         new Setting(containerEl)
-            .setName('API Configuration')
+            .setName('API configuration')
             .setHeading();
 
         new Setting(containerEl)
-            .setName('Google API Key')
-            .setDesc('Enter your Google API key')
+            .setName('Gemini API key')
+            .setDesc('Enter your Gemini API key')
             .addText(text => text
                 .setPlaceholder('Enter your API key')
                 .setValue(this.plugin.settings.googleApiKey)
@@ -39,7 +31,7 @@ export class SettingsTab extends PluginSettingTab {
                 }));
 
         new Setting(containerEl)
-            .setName('DeepSeek API Key')
+            .setName('DeepSeek API key')
             .setDesc('Enter your DeepSeek API key')
             .addText(text => text
                 .setPlaceholder('Enter your API key')
@@ -50,7 +42,7 @@ export class SettingsTab extends PluginSettingTab {
                 }));
 
         new Setting(containerEl)
-            .setName('API Provider')
+            .setName('API provider')
             .setDesc('Choose your API provider')
             .addDropdown(dropdown => {
                 dropdown
@@ -63,19 +55,5 @@ export class SettingsTab extends PluginSettingTab {
                     });
             });
 
-        new Setting(containerEl)
-            .setName('Plugin Actions')
-            .setHeading();
-
-        new Setting(containerEl)
-            .setName('Reload Plugin')
-            .setDesc('Click to reload the plugin and apply the new API key.')
-            .addButton(button => button
-                .setButtonText('Reload')
-                .onClick(async () => {
-                    await this.plugin.reloadPlugin();
-                    containerEl.empty();
-                    this.display();
-                }));
     }
 }
