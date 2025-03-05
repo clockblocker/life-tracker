aa796833168f579264cac964ea3a180feeeb1776
clockblocker <asde3211q@gmail.com>
2025-03-05 06:19:25 +0100

    enhace the traslation function

--- src/prompts/translate-de-to-eng.ts
+++ src/prompts/translate-de-to-eng.ts
@@ -1,1 +1,31 @@
-export const translate_de_to_eng = `Translate the given German text to English`;
+export const translate_de_to_eng = `Translate the given German text to English. When possible, keep Obsidan-flavored markdown formatting semi-consistent between original and translation. If it requires you to bend english grammar a bit -- so be it. If there is no [[]] or ==...== in the sentence, translate it as naturally-sounging as possible.
+<examples>
+<example>
+<user_input>Wir treffen uns [[entweder]] im Restaurant, [[oder]] im *Kino*.</user_input>
+<agent_input>We'll meet [[either]] at the restaurant [[or]] at the *cinema*.<agent_input>
+</example>
+<example>
+<user_input>Er ist zwar noch jung, [[aber]] schon sehr ==berühmt==.</user_input>
+<agent_input>He is indeed still young, [[but]] already very ==famous==.<agent_input>
+</example>
+<example>
+<user_input>Es kam aber den Vögeln [[in den Sinn]], sie wollten nicht länger ohne Herrn sein und einen unter sich zu ihrem König wählen.</user_input>
+<agent_input>But it [[occurred]] to the birds that they no longer wanted to be without a master and wanted to choose one among themselves to be their king.<agent_input>
+</example>
+<example>
+<user_input>Du willst dein Lebtag nicht wieder allein vom Wege [[ab]] in den Wald laufen, wenn dir's die Mutter verboten hat.</user_input>
+<agent_input>You will never again stray [[away]] from the path into the forest alone in your life if your mother has forbidden it.<agent_input>
+</example>
+
+<example>
+<user_input>Die Dursleys besaßen alles, was sie wollten, doch sie hatten auch ein Geheimnis, und dass [[es]] jemand aufdecken könnte, war ihre größte Sorge.</user_input>
+<agent_input>The Dursleys possessed everything they wanted, but they also had a secret, and their greatest fear was that somebody would uncover [[it]].<agent_input>
+</example>
+
+<example>
+<user_input>[[Es]] besteht die Möglichkeit ==einer== Katastrophe.</user_input>
+<agent_input>[[There]] is a possibility ==of the== disaster.<agent_input>
+</example>
+</examples>
+`;
+
