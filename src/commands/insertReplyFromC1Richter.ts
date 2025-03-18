5700e173ac44129f3b5e1bda7962c65dfa12edc3
clockblocker <asde3211q@gmail.com>
2025-03-18 12:55:08 +0100

    enhanced schribing-checker

--- src/commands/insertReplyFromC1Richter.ts
+++ src/commands/insertReplyFromC1Richter.ts
@@ -3,9 +3,10 @@ import TextEaterPlugin from '../main';
 
 export default async function insertReplyFromC1Richter(plugin: TextEaterPlugin, editor: Editor, selection: string, v2?: boolean) {
     try {
-        const response = await plugin.apiService.consultC1Richter(selection, v2);
+        const noBrackets = selection.trim().replaceAll("[[", "").replaceAll("]]", "");
+        const response = await plugin.apiService.consultC1Richter(noBrackets, v2);
         if (response) {
-            editor.replaceSelection(selection + '\n' + response.trim() + '\n');
+            editor.replaceSelection(selection + '\n' + response.trim());
         }
     } catch (error) {
         console.error('Error in C1 Richter command:', error);
