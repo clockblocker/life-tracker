d14db762a9767b39cbc38702fb46f7d3db0bc4db
clockblocker <asde3211q@gmail.com>
2025-03-31 08:56:36 +0200

    eslint-plugin-prettier works

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'eslint/config';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
@@ -16,11 +17,16 @@ export default defineConfig([
 		},
 		plugins: {
 			'@typescript-eslint': tsPlugin,
+			// Add the Prettier plugin here
+			prettier: prettierPlugin,
 		},
+		// Extend the Prettier config to disable conflicting ESLint rules
 		extends: [prettierConfig],
 		rules: {
 			semi: 'error',
 			'prefer-const': 'error',
+			// Run Prettier as an ESLint rule and report issues as errors
+			'prettier/prettier': 'error',
 		},
 	},
 ]);
